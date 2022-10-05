a8b14e4c63b2929eb050e156a95e038ab3ec9701
Mike McBride <mike.mcbride@hey.com>
2022-10-04 22:52:28 -0500

    adds husky and lint-staged to run prettier on all files on commit

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,31 +1,37 @@
 <template>
-  <div class="mb-4" :class="wrapperClass">
-    <label v-if="label" class="block text-sm font-semibold text-gray-900 mb-1" :for="fieldName">{{ label }}</label>
-    <div>
-        <input
-            v-bind="$attrs"
-            :name="fieldName"
-            :id="fieldName"
-            :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
-            class="shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent block w-full sm:text-sm border border-gray-300 rounded-md appearance-none bg-white py-2 px-3"
-            :class="inputClass" />
+    <div class="mb-4" :class="wrapperClass">
+        <label
+            v-if="label"
+            class="block text-sm font-semibold text-gray-900 mb-1"
+            :for="fieldName"
+            >{{ label }}</label
+        >
+        <div>
+            <input
+                v-bind="$attrs"
+                :name="fieldName"
+                :id="fieldName"
+                :value="modelValue"
+                @input="$emit('update:modelValue', $event.target.value)"
+                class="shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent block w-full sm:text-sm border border-gray-300 rounded-md appearance-none bg-white py-2 px-3"
+                :class="inputClass"
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'BaseInput',
-  props: ['modelValue', 'label', 'inputClass', 'wrapperClass'],
-  inheritAttrs: false,
-  computed: {
-      fieldName() {
-        if (this.label) {
-          return this.label.toLowerCase().split(' ').join('-')
-        }
-      }
-  },
+    name: 'BaseInput',
+    props: ['modelValue', 'label', 'inputClass', 'wrapperClass'],
+    inheritAttrs: false,
+    computed: {
+        fieldName() {
+            if (this.label) {
+                return this.label.toLowerCase().split(' ').join('-')
+            }
+        },
+    },
 }
 </script>
 
@@ -37,7 +43,7 @@ input::-webkit-inner-spin-button {
     margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
 }
 
-input[type=number] {
-    -moz-appearance:textfield; /* Firefox */
+input[type='number'] {
+    -moz-appearance: textfield; /* Firefox */
 }
 </style>
--- src/getShadowStyles.js
+++ src/getShadowStyles.js
@@ -1,8 +1,12 @@
-export default function(shadows) {
-  const styles = shadows.filter(s => s.active).map(s => {
-    let prefix = s.inset ? 'inset ' : ''
-    return `${prefix}${s.offsetX}px ${s.offsetY}px ${s.blur}px ${s.spread}px ${s.color}`
-  }).join(',').trim()
+export default function (shadows) {
+    const styles = shadows
+        .filter((s) => s.active)
+        .map((s) => {
+            let prefix = s.inset ? 'inset ' : ''
+            return `${prefix}${s.offsetX}px ${s.offsetY}px ${s.blur}px ${s.spread}px ${s.color}`
+        })
+        .join(',')
+        .trim()
 
-  return `box-shadow: ${styles};`
+    return `box-shadow: ${styles};`
 }
